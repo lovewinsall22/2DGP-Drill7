b8243b60ef9268d3def09f1a90f6d15a863d0289
lovewinsall22 <taehyun0222@naver.com>
2025-09-30 15:15:59 +0900

    기본 틀

--- boy_grass_object_recent.py
+++ boy_grass_object_recent.py
@@ -19,6 +19,18 @@ def handle_events():
 
 open_canvas()
 
+# 초기화 코드
+reset_world()
+
+# 게임루프 코드
+while running:
+    # 입력 처리
+    handle_events()
+    # 게임 로직
+    update_world()
+    # 렌더링
+    render_world()
+    delay(0.05)
 
 
 close_canvas()
